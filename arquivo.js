236b289f6fbdecc20953a8ad5f23d2dc95d93c30
Edoardo paz <pazedoardo@gmail.com>
2021-07-11 07:26:11 -0300

    Adicionei o index

---
+++ arquivo.js
@@ -0,0 +1,67 @@
+var erros = []
+
+var sorteado = Math.floor(Math.random()* 100) + 1
+
+
+const CHANCES = 6
+
+
+function apostarNumero() {
+    var inNUmero = document.getElementById('innumero')
+    var numero = Number(inNUmero.value)
+
+
+    if(numero < 0 || numero > 100 || isNaN(numero) || numero == 0) {
+        alert('Porfavor digite um numero valido..')
+        innumero.value = ""
+        innumero.focus()
+        return
+    }
+
+
+    var outErros = document.getElementById('outErros')
+    var outChances = document.getElementById('outChances')
+    var outDica = document.getElementById('outDica')
+
+
+    if(numero == sorteado) {
+        alert('Parabens voce acertou!!!')
+        btaposta.disable = true
+        btjogar.className = "Exibe"
+        outDica.textContent = ` Parabens!! Número sorteado: ${numero}`
+    } else if(erros.indexOf(numero) >=0){
+        alert('Você já apostou o numero:' + numero + ". Tente outro...")
+    } else {
+        erros.push(numero)
+        var numErros = erros.length
+    
+
+
+  var numChances = CHANCES - numErros
+
+  outErros.textContent = numErros + "(" + erros.join(',') + ")"
+  outChances.textContent = numChances
+
+  if(numChances == 0) {
+      alert('Suas chances acabaram...')
+      btaposta.disable = true
+      btjogar.className = "Exibe"
+      outDica.textContent = "Game over!! Numero sorteado:" + sorteado
+  } else {
+      var dica = numero < sorteado ? "Maior" : "Menor";
+      outDica.textContent = `Dica tente um  numero ${dica}  Que  ${numero}`
+  }
+
+    }
+    innumero.value = ""
+    innumero.focus()
+
+}
+var btsorteado = document.getElementById('btaposta')
+btsorteado.addEventListener('click', apostarNumero)
+
+function jogarDenovo() {
+    location.reload()
+}
+var btreload = document.getElementById('btjogar')
+btreload.addEventListener('click', jogarDenovo)
